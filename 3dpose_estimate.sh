cd2f98a28640125f4e034000a12ca5e68d108499
retnrernet <49436790+retnrernet@users.noreply.github.com>
2020-01-15 14:13:28 +0300

    Update 3dpose_estimate.sh

--- 3dpose_estimate.sh
+++ 3dpose_estimate.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-
-for f in keras_Realtime_Multi-Person_Pose_Estimation/sample_images/*; do
-
-	filename=$(basename -- "$f")
-  no_ext="${filename%.*}"
-  
-  echo "Processing $no_ext"
-  
-  python2 hmr/demo.py --img_path $f \
-                     --json_path keras_Realtime_Multi-Person_Pose_Estimation/sample_jsons/$no_ext.json  
+   
+  python2 hmr/demo.py --json_path alphapose-results.json  
   
 done
 
